bdbf911855deccf3533a3dad12e7ccf40676edab
segunjkf <sege.timz12@gmail.com>
2022-07-17 22:21:58 +0100

    updated website

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -95,3 +95,56 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = true
   }
 }
+
+resource "aws_cloudfront_distribution" "root_s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.root_bucket.website_endpoint
+    origin_id = "S3-.${var.bucket_name}"
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+
+  aliases = [var.domain_name]
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "S3-.${var.bucket_name}"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["Origin"]
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl = 0
+    default_ttl = 0
+    max_ttl = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+  tags = var.common_tags
+}
