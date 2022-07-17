c12380d23510991ec0090605a48e9872a3f072b6
segunjkf <sege.timz12@gmail.com>
2022-07-17 07:33:31 +0100

    starting apigateway

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,4 +1,4 @@
-locals {
+/*locals {
     lambda_zip_location = "/lambda_function.zip"
 }
 
@@ -6,14 +6,14 @@ data "archive_file" "registercount" {
   type        = "zip"
   source_file = "lambda_function.py"
   output_path = "${local.lambda_zip_location}"
-}
+}*/
 
 resource "aws_lambda_function" "lambda_function" {
-  filename      = "${local.lambda_zip_location}"
+  filename      = "lambda_function.zip"
   function_name = "lambda_function"
   role          = "${aws_iam_role.lambda_role.arn}"
   handler       = "lambda_function.lambda_handler"
-  source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
+  source_code_hash = filebase64sha256("lambda_function.zip")
   runtime = "python3.8"
 
 }
