5eff87174024bddde795766ec3d14719ce6cd9fe
segunjkf <sege.timz12@gmail.com>
2022-07-17 06:16:53 +0100

    lambda

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -1,21 +1,19 @@
-resource "aws_lambda_function" "registercount" {
-  # If the file is not in the current working directory you will need to include a 
-  # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+locals {
+    lambda_zip_location = "outputs/dbcall.zip"
+}
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+data "archive_file" "dbcall" {
+  type        = "zip"
+  source_file = "registercount.py"
+  output_path = "${local.lambda_zip_location}"
+}
 
-  runtime = "nodejs12.x"
+resource "aws_lambda_function" "lambda_func" {
+  filename      = "${local.lambda_zip_location}"
+  function_name = "dbcall"
+  role          = "${aws_iam_role.lambda_role.arn}"
+  handler       = "dbcall.lambda_handler"
+  source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
+  runtime = "python3.8"
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
 }
