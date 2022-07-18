840a099868726460ce32c1657368035b0fd0b2fb
segunjkf <sege.timz12@gmail.com>
2022-07-18 15:56:44 +0100

    website misconfig

--- modules/s3-static/main.tf
+++ modules/s3-static/main.tf
@@ -4,7 +4,18 @@ resource "aws_s3_bucket" "static" {
   acl    = "public-read"
   policy =  templatefile("template/s3-public-json.json", { bucket = var.bucket_name })
   force_destroy = true
-  
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["*"]
+    max_age_seconds = 10
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+    
+  }
 }
 
 
@@ -20,6 +31,12 @@ resource "aws_s3_bucket" "root_static" {
   acl    = "public-read"
   policy =  templatefile("template/s3-public-json.json", { bucket = "root-${var.bucket_name}" })
   force_destroy = true
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["*"]
+    max_age_seconds = 10
+  }
    website {
     redirect_all_requests_to = aws_s3_bucket.static.bucket_regional_domain_name
 
